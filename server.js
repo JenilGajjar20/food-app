b1968a379c3981c0f8f22a6825e5faf1f8149307
Jenil Gajjar <68738624+JenilGajjar20@users.noreply.github.com>
2023-01-07 19:42:40 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const ejs = require("ejs");
+const expressLayout = require("express-ejs-layouts");
+
+// Creating an express app
+const app = express();
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+// Set the template engine
+app.use(expressLayout);
+app.set("views", path.join(__dirname, "/resources/views"));
+app.set("view engine", "ejs");
+
+// Define a PORT
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started at port http://localhost:${PORT}`);
+});
