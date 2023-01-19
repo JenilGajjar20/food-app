d4d58e58bc9d56b7fd8da4f951404d118df0759f
Jenil Gajjar <68738624+JenilGajjar20@users.noreply.github.com>
2023-01-19 18:38:46 +0530

    login, register and cart page created

--- server.js
+++ server.js
@@ -6,17 +6,29 @@ const expressLayout = require("express-ejs-layouts");
 // Creating an express app
 const app = express();
 
-// Assets
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
 
 // Set the template engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
+
+// Assets
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 
 // Define a PORT
 const PORT = process.env.PORT || 3000;
