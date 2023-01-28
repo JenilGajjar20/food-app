012c276714ce13e41ddc8d8a20607a883bec6d43
Jenil Gajjar <68738624+JenilGajjar20@users.noreply.github.com>
2023-01-28 11:55:44 +0530

    order status updated

--- routes/web.js
+++ routes/web.js
@@ -4,7 +4,8 @@ const cartController = require("../app/http/controllers/customers/cartController
 const orderController = require("../app/http/controllers/customers/orderController");
 
 // Admin Controller
-const AdminOrderController = require("../app/http/controllers/admin/orderController");
+const adminOrderController = require("../app/http/controllers/admin/orderController");
+const statusController = require("../app/http/controllers/admin/statusController");
 
 // Middleware
 const guest = require("../app/http/middlewares/guest");
@@ -32,7 +33,8 @@ function initRoutes(app) {
   app.get("/customer-orders", auth, orderController().index);
 
   // Admin Routes
-  app.get("/admin-orders", admin, AdminOrderController().index);
+  app.get("/admin-orders", admin, adminOrderController().index);
+  app.post("/admin/order/status", admin, statusController().update);
 }
 
 module.exports = initRoutes;
